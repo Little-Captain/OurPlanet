dcfa20c1211fd5d90c388e25431cc5e4fceea155
Little-Captain <littlecaptain@foxmail.com>
2018-09-19 22:19:42 +0800

    展示categories

--- OurPlanet/CategoriesViewController.swift
+++ OurPlanet/CategoriesViewController.swift
@@ -25,28 +25,44 @@ import RxSwift
 import RxCocoa
 
 class CategoriesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-
-  @IBOutlet var tableView: UITableView!
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-
-    startDownload()
-  }
-
-  func startDownload() {
     
-  }
-  
-  // MARK: UITableViewDataSource
-  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 0
-  }
-  
-  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: "categoryCell")!
-    return cell
-  }
-  
+    @IBOutlet var tableView: UITableView!
+    
+    let categories = Variable<[EOCategory]>([])
+    let bag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        categories
+            .asObservable()
+            .subscribe(onNext: { [weak self] _ in
+                DispatchQueue.main.async {
+                    self?.tableView?.reloadData()
+                }
+            })
+            .disposed(by: bag)
+        startDownload()
+    }
+    
+    func startDownload() {
+        EONET
+            .categories
+            .bind(to: categories)
+            .disposed(by: bag)
+    }
+    
+    // MARK: UITableViewDataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories.value.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "categoryCell")!
+        let category = categories.value[indexPath.row]
+        cell.textLabel?.text = category.name
+        cell.detailTextLabel?.text = category.description
+        return cell
+    }
+    
 }
 
