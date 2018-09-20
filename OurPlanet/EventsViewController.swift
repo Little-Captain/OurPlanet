2a46b46edd1a9079f9c88c35231af7fbb0d4afac
Little-Captain <littlecaptain@foxmail.com>
2018-09-20 14:12:09 +0800

    展示分类下的事件列表

--- OurPlanet/EventsViewController.swift
+++ OurPlanet/EventsViewController.swift
@@ -24,29 +24,39 @@ import UIKit
 import RxSwift
 
 class EventsViewController : UIViewController, UITableViewDataSource {
-
-  @IBOutlet var tableView: UITableView!
-  @IBOutlet var slider: UISlider!
-  @IBOutlet var daysLabel: UILabel!
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-
-    tableView.rowHeight = UITableViewAutomaticDimension
-    tableView.estimatedRowHeight = 60
-  }
-
-  @IBAction func sliderAction(slider: UISlider) {
-  }
-  
-  // MARK: UITableViewDataSource
-  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 0
-  }
-  
-  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: "eventCell") as! EventCell
-    return cell
-  }
-  
+    
+    @IBOutlet var tableView: UITableView!
+    @IBOutlet var slider: UISlider!
+    @IBOutlet var daysLabel: UILabel!
+    
+    let events = Variable<[EOEvent]>([])
+    let bag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 60
+        
+        events
+            .asObservable()
+            .subscribe(onNext: { [weak self] _ in self?.tableView?.reloadData() })
+            .disposed(by: bag)
+    }
+    
+    @IBAction func sliderAction(slider: UISlider) {
+    }
+    
+    // MARK: UITableViewDataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return events.value.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "eventCell") as! EventCell
+        let event = events.value[indexPath.row]
+        cell.configure(event: event)
+        return cell
+    }
+    
 }
