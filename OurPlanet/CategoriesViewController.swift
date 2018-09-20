0c7cfa6c762fc81c061d1e94fa43af580c7dd78d
Little-Captain <littlecaptain@foxmail.com>
2018-09-20 21:44:34 +0800

    使用两种方法显示获取Events的进度, 并使用share()解决一个bug

--- OurPlanet/CategoriesViewController.swift
+++ OurPlanet/CategoriesViewController.swift
@@ -28,6 +28,7 @@ class CategoriesViewController: UIViewController, UITableViewDataSource, UITable
     
     @IBOutlet var tableView: UITableView!
     var activityIndicator: UIActivityIndicatorView!
+    let download = DownloadView()
     
     let categories = Variable<[EOCategory]>([])
     let bag = DisposeBag()
@@ -39,6 +40,11 @@ class CategoriesViewController: UIViewController, UITableViewDataSource, UITable
         activityIndicator.color = .black
         navigationItem.rightBarButtonItem = UIBarButtonItem(customView: activityIndicator)
         activityIndicator.startAnimating()
+        
+        view.addSubview(download)
+        view.layoutIfNeeded()
+        
+        tableView.tableFooterView = UIView()
         
         categories
             .asObservable()
@@ -53,16 +59,19 @@ class CategoriesViewController: UIViewController, UITableViewDataSource, UITable
     }
     
     func startDownload() {
-        let eoCategories = EONET.categories
+        download.progress.progress = 0
+        download.label.text = "Download: 0%"
         
+        let eoCategories = EONET.categories
         let downloadedEvents = eoCategories
             .flatMap { Observable.from($0.map { EONET.events(category: $0) }) }
             .merge(maxConcurrent: 2)
         
+        // Challenge 2: 方法 1
         let updatedCategories = eoCategories
             .flatMap {
-                downloadedEvents.scan($0) { updated, events in
-                    updated.map {
+                downloadedEvents.scan((0, $0)) { tuple, events in
+                    (tuple.0 + 1, tuple.1.map {
                         let eventsForCategory = EONET.filteredEvents(events: events, forCategory: $0)
                         if !eventsForCategory.isEmpty {
                             var c = $0
@@ -70,17 +79,82 @@ class CategoriesViewController: UIViewController, UITableViewDataSource, UITable
                             return c
                         }
                         return $0
-                    }
+                    })
                 }
             }
-            .do(onCompleted: { [weak self] in
-                DispatchQueue.main.async { self?.activityIndicator?.stopAnimating() }
+            .do(onNext: { [weak self] tuple in
+                DispatchQueue.main.async {
+                    let progress = Float(tuple.0) / Float(tuple.1.count)
+                    self?.download.progress.progress = progress
+                    let percent = Int(progress * 100.0)
+                    self?.download.label.text = "Download: \(percent)%"
+                }
             })
-        
+            .do(onCompleted: { [weak self] in
+                DispatchQueue.main.async {
+                    self?.activityIndicator?.stopAnimating()
+                    self?.download.isHidden = true
+                }
+            })
+
         eoCategories
-            .concat(updatedCategories)
+            .concat(updatedCategories.map { $0.1 })
             .bind(to: categories)
             .disposed(by: bag)
+        
+        // Challenge 2: 方法 2
+//        let updatedCategories = eoCategories
+//            .flatMap {
+//                downloadedEvents.scan($0) { updated, events in
+//                    updated.map {
+//                        let eventsForCategory = EONET.filteredEvents(events: events, forCategory: $0)
+//                        if !eventsForCategory.isEmpty {
+//                            var c = $0
+//                            c.events += eventsForCategory
+//                            return c
+//                        }
+//                        return $0
+//                    }
+//                }
+//            }
+//            // 这里必须使用 share() 共享订阅. share() 会共享已经创建的订阅给所有订阅者
+//            // 不然会因为重复订阅而产生重复的网络请求(所有的分类事件都请求两次)
+//            // 为什么这里能使用 share() 解决这个 bug
+//            // 1. 所有针对 updatedCategories 的订阅都是同时进行的
+//            // 2. updatedCategories 完成后, 没有新的订阅
+//            .share()
+//            .do(onCompleted: { [weak self] in
+//                DispatchQueue.main.async {
+//                    self?.activityIndicator?.stopAnimating()
+//                    self?.download.isHidden = true
+//                }
+//            })
+//
+//        eoCategories
+//            .flatMap { categories in
+//                updatedCategories
+//                    .scan(0) { count, _ in count + 1 }
+//                    .startWith(0)
+//                    .map { ($0, categories.count) }
+//            }
+//            .subscribe(onNext: { [weak self] tuple in
+//                DispatchQueue.main.async {
+//                    let progress = Float(tuple.0) / Float(tuple.1)
+//                    self?.download.progress.progress = progress
+//                    let percent = Int(progress * 100.0)
+//                    self?.download.label.text = "Download: \(percent)%"
+//                }
+//            })
+//            .disposed(by: bag)
+//
+//        eoCategories
+//            .concat(updatedCategories)
+//            .bind(to: categories)
+//            .disposed(by: bag)
+        
+        // 方法 1 和 方法 2 比较
+        // 方法 2 的进度更新在列表刷新之前
+        // 方法 2 所有的事件请求都发送了两次请求(重复发送请求), 使用 share() 解决这个 bug
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
